1d0ba0260344ea50eec76a23f6406c9334b952f7
timraay <traaymakers2005@gmail.com>
2021-11-01 22:58:27 +0100

    Update __init__.py

--- aiorcon/__init__.py
+++ aiorcon/__init__.py
@@ -1,10 +1,4 @@
-import logging
-
 from .rcon import RCON
 from .exceptions import (RCONError, RCONAuthenticationError, RCONConnectionError, RCONTimeoutError,
                         RCONClosedError, RCONCommunicationError, RCONMessageError, RCONStateError)
 __version__ = '0.6.8'
-
-log = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: %(message)s',
-                    datefmt="[%d/%m/%Y %H:%M]")
